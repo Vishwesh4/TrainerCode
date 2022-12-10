added more wandb utilites such as sweeps, watch gradients, and few bug changes

trainer/dataset.py
@@ -2,7 +2,7 @@
 This script is a wrapper around the data loader. The class defined returns data loaders
 """
 import abc
-from typing import Any, List, Optional, Union, Tuple
+from typing import Any, List, Optional, Union, Tuple, Dict
 
 from torch.utils.data import Dataset, DataLoader
 
@@ -42,6 +42,13 @@ class Dataset(RegistrantFactory):
             self.test_loader,
         ) = self.get_loaders()
 
+    def _inject_args(self,args:Dict) -> None:
+        """
+        Injects all the hyperparameters into class variable
+        """
+        self.system_params = args
+    
+    
     @abc.abstractmethod
     def get_transforms(self) -> Tuple[Any, Any]:
         """

trainer/model.py
@@ -1,5 +1,5 @@
 import abc
-from typing import List
+from typing import List, Dict
 import warnings
 
 import torch
@@ -46,5 +46,13 @@ class Model(RegistrantFactory, nn.Module):
                 warnings.warn(f"Warning... No weight could be loaded..\n{not_loaded}")
             else:
                 warnings.warn(f"Warning... Some Weights could not be loaded\n{not_loaded}")
+        else:
+            print("All weights successfully loaded")
         model_dict.update(weights)
         self.load_state_dict(model_dict)
+
+    def _inject_args(self,args:Dict) -> None:
+        """
+        Injects all the hyperparameters into class variable
+        """
+        self.system_params = args